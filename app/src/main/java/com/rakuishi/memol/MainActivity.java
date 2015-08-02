c121e0cd110537d0814777272e06b28ac1bff586
rakuishi <rakuishi@gmail.com>
2015-08-02 14:48:20 +0900

    First commit

---
+++ app/src/main/java/com/rakuishi/memol/MainActivity.java
@@ -0,0 +1,36 @@
+package com.rakuishi.memol;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toolbar;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setActionBar((Toolbar) findViewById(R.id.toolbar));
+        setTitle(R.string.app_name);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
